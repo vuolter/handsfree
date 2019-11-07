f0daf8b780f34844d4f877a85e4de0022abd6997
Oz Ramos <heyozramos@gmail.com>
2019-11-07 10:50:43 -0800

    #7 Adds the SmileTile route

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template lang="pug">
   v-app
+    v-navigation-drawer.d-print-none(v-model='sidebar.main' app)
+      v-layout(column fill-height)
+        v-list(dense)
+          v-list-item(:to='{name: "Home"}' exact)
+            v-list-item-action
+              v-icon mdi-home
+            v-list-item-content Home
+          v-list-group(prepend-icon='mdi-gamepad-variant-outline')
+            template(v-slot:activator)
+              v-list-item-content Experiments
+            v-list-item(:to='{name: "SmileTiles"}')
+              v-list-item-action
+                v-icon mdi-emoticon-excited-outline
+              v-list-item-content Smile Tiles
+
     v-app-bar(app)
       div.d-flex.align-center
         div(style='width: 60px')
@@ -19,7 +34,7 @@ import { mapState } from 'vuex'
 export default {
   components: { TensorMonkey },
 
-  computed: mapState(['handsfree', 'isTracking']),
+  computed: mapState(['handsfree', 'isTracking', 'sidebar']),
 
   data: () => ({}),
 
--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,15 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home
+  },
+  {
+    path: '/smile-tiles',
+    name: 'SmileTiles',
+    component: () =>
+      import(/* webpackChunkName: "SmileTiles" */ '../views/SmileTiles.vue')
   }
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
