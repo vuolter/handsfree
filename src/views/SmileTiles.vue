e88995acab143783c050b037e9495df8876754f5
Oz Ramos <heyozramos@gmail.com>
2019-11-07 11:08:11 -0800

    #7 Adds a grid of clickable tiles

--- src/views/SmileTiles.vue
+++ src/views/SmileTiles.vue
@@ -1,3 +1,36 @@
 <template lang="pug">
-  h1 SmileTiles
+  v-container
+    v-card(color='indigo')
+      v-card-text
+        v-layout(wrap)
+          v-row
+            v-col.col-12.col-sm-4
+              v-card
+                v-card-title Best
+            v-col.col-12.col-sm-4
+              v-card
+                v-card-title Score
+            v-col.col-12.col-sm-4
+              v-card
+                v-card-title Time
+
+        v-layout
+          v-row
+            v-col.md-6
+        v-card(color='indigo lighten-3' style='max-width: 500px; margin: auto')
+          v-card-text
+            v-layout
+              v-row(dense)
+                v-col.col-3(v-for='index in 16' :key='index')
+                  v-card(style="height: 80px;" ref='tile' @click='clickedTile(index)')
 </template>
+
+<script>
+export default {
+  methods: {
+    clickedTile(index) {
+      console.log('clicked', index)
+    }
+  }
+}
+</script>
