46bf021d9afc472940d87e76c9201eb2baae824a
Oz Ramos <heyozramos@gmail.com>
2019-11-07 11:57:17 -0800

    #7 Clicking on black tiles shows a new tile

--- src/views/SmileTiles.vue
+++ src/views/SmileTiles.vue
@@ -22,14 +22,59 @@
             v-layout
               v-row(dense)
                 v-col.col-3(v-for='index in 16' :key='index')
-                  v-card(style="height: 80px;" ref='tile' @click='clickedTile(index)')
+                  v-card(style="height: 80px;" ref='tile' @click='clickedTile(index - 1)' :color='tiles[index - 1] | tileColor(index)')
 </template>
 
 <script>
+import { random } from 'lodash'
+
 export default {
+  filters: {
+    tileColor(tileValue) {
+      let color = 'white'
+
+      if (tileValue) {
+        color = 'black'
+      }
+
+      return color
+    }
+  },
+
+  data: () => ({
+    tiles: Array.from({ length: 16 }, () => 0)
+  }),
+
+  /**
+   * Choose 3 items on random
+   */
+  mounted() {
+    window.view = this
+
+    for (let i = 0; i < 3; i++) {
+      this.setRandomTile()
+    }
+  },
+
   methods: {
     clickedTile(index) {
-      console.log('clicked', index)
+      if (this.tiles[index] > 0) {
+        this.setRandomTile()
+        this.tiles[index] -= 1
+      }
+    },
+
+    /**
+     * Sets a random white tile to a random grid type
+     */
+    setRandomTile() {
+      let index = random(0, 15)
+
+      if (this.tiles[index]) {
+        return this.setRandomTile()
+      }
+
+      this.$set(this.tiles, index, 1)
     }
   }
 }
