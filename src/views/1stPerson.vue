d9ffe9ce348a32a796153b9b8a90ebfa936ff863
Oz Ramos <heyozramos@gmail.com>
2019-11-13 18:39:56 -0800

    #19 Handle leaning in/out

--- src/views/1stPerson.vue
+++ src/views/1stPerson.vue
@@ -18,6 +18,10 @@ export default {
       left: false,
       right: false,
       jump: false
+    },
+
+    velocity: {
+      y: 0
     }
   }),
 
@@ -43,14 +47,34 @@ export default {
       window.Handsfree.use('threeCamera', (pointer, instance) => {
         // The normalized amount from center to lean before moving in that directon
         let strafeBuffer = 0.15
+        let leanBuffer = {
+          out: 0.1,
+          in: 0.01
+        }
 
-        // Move
+        // Strafe
         if (instance.head.translation[0] > 0.5 + strafeBuffer)
           this.move.right = true
         else this.move.right = false
+
         if (instance.head.translation[0] < 0.5 - strafeBuffer)
           this.move.left = true
         else this.move.left = false
+
+        // Lean
+        if (instance.head.translation[2] > 0.25 + leanBuffer.out)
+          this.move.forward = true
+        else this.move.forward = false
+
+        if (instance.head.translation[2] < 0.25 - leanBuffer.in)
+          this.move.backward = true
+        else this.move.backward = false
+
+        // Jump
+        if (instance.head.state.browsUp) {
+          if (this.move.jump) this.velocity.y += 350
+          this.move.jump = false
+        }
 
         console.log(instance.head.translation)
       })
@@ -72,9 +96,6 @@ export default {
           var camera, scene, renderer, controls
           var objects = []
           var raycaster
-          var moveForward = false
-          var moveBackward = false
-          var canJump = false
           var prevTime = performance.now()
           var velocity = new THREE.Vector3()
           var direction = new THREE.Vector3()
@@ -116,36 +137,7 @@ export default {
               instructions.style.display = ''
             })
             scene.add(controls.getObject())
-            var onKeyDown = function(event) {
-              switch (event.keyCode) {
-                case 38: // up
-                case 87: // w
-                  moveForward = true
-                  break
-                case 40: // down
-                case 83: // s
-                  moveBackward = true
-                  break
-                case 32: // space
-                  if (canJump === true) velocity.y += 350
-                  canJump = false
-                  break
-              }
-            }
-            var onKeyUp = function(event) {
-              switch (event.keyCode) {
-                case 38: // up
-                case 87: // w
-                  moveForward = false
-                  break
-                case 40: // down
-                case 83: // s
-                  moveBackward = false
-                  break
-              }
-            }
-            document.addEventListener('keydown', onKeyDown, false)
-            document.addEventListener('keyup', onKeyUp, false)
+
             raycaster = new THREE.Raycaster(
               new THREE.Vector3(),
               new THREE.Vector3(0, -1, 0),
@@ -248,25 +240,26 @@ export default {
               var delta = (time - prevTime) / 1000
               velocity.x -= velocity.x * 10.0 * delta
               velocity.z -= velocity.z * 10.0 * delta
-              velocity.y -= 9.8 * 100.0 * delta // 100.0 = mass
-              direction.z = Number(moveForward) - Number(moveBackward)
+              this.velocity.y -= 9.8 * 100.0 * delta // 100.0 = mass
+              direction.z =
+                Number(this.move.forward) - Number(this.move.backward)
               direction.x = Number(this.move.right) - Number(this.move.left)
               direction.normalize() // this ensures consistent movements in all directions
-              if (moveForward || moveBackward)
+              if (this.move.forward || this.move.backward)
                 velocity.z -= direction.z * 400.0 * delta
               if (this.move.left || this.move.right)
                 velocity.x -= direction.x * 400.0 * delta
               if (onObject === true) {
-                velocity.y = Math.max(0, velocity.y)
-                canJump = true
+                this.velocity.y = Math.max(0, this.velocity.y)
+                this.move.jump = true
               }
               controls.moveRight(-velocity.x * delta)
               controls.moveForward(-velocity.z * delta)
-              controls.getObject().position.y += velocity.y * delta // new behavior
+              controls.getObject().position.y += this.velocity.y * delta // new behavior
               if (controls.getObject().position.y < 10) {
-                velocity.y = 0
+                this.velocity.y = 0
                 controls.getObject().position.y = 10
-                canJump = true
+                this.move.jump = true
               }
               prevTime = time
             }
