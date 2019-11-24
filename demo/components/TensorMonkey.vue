494a87038657372ba4167e37505e80ca9d8d05ca
Oz Ramos <heyozramos@gmail.com>
2019-11-23 17:14:01 -0800

    #0 makes monkey head match users pov

--- demo/components/TensorMonkey.vue
+++ demo/components/TensorMonkey.vue
@@ -1,28 +1,65 @@
 <template lang="pug">
   div
-    TensorMonkey.my-5(v-if='!isTracking' :style='styles')
-    .my-5(v-else :style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
-      TensorMonkey.ozramos-tensormonkey-animated.my-5(:style='styles')
+    Logo.my-5(v-if='!isTracking' :style='computedStyles')
+    .my-5(v-else :style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
+      Logo.ozramos-tensormonkey-animated.my-5(:style='computedStyles')
 </template>
 
 <script>
-import TensorMonkey from '@/components/Logo'
+import Logo from '@/components/Logo'
 import { mapState } from 'vuex'
 
 export default {
   props: ['styles'],
-  computed: mapState(['isTracking']),
-  components: { TensorMonkey }
+  computed: {
+    ...mapState(['isTracking']),
+    computedStyles() {
+      console.log(
+        this.styles +
+          ';' +
+          `transform: perspective(1000px) rotateX(${this.pov.pitch}rad) rotateY(${this.pov.yaw}rad) rotateZ(${this.pov.roll}rad)`
+      )
+
+      return (
+        this.styles +
+        ';' +
+        `transform: perspective(1000px) rotateX(${this.pov.pitch}rad) rotateY(${this.pov.yaw}rad) rotateZ(${this.pov.roll}rad)`
+      )
+    }
+  },
+  components: { Logo },
+  data: () => ({
+    id: Math.random(),
+    pov: {
+      pitch: 0,
+      yaw: 0,
+      roll: 0
+    }
+  }),
+
+  mounted() {
+    this.id = Math.random()
+
+    window.Handsfree.use(`tensormonkey.puppet-${this.id}`, ({ head }) => {
+      this.pov.pitch = -head.rotation[0]
+      this.pov.yaw = head.rotation[1]
+      this.pov.roll = -head.rotation[2]
+    })
+  },
+
+  beforeDestroy() {
+    window.Handsfree.disable(`tensormonkey.puppet-${this.id}`)
+  }
 }
 </script>
