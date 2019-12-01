a44d4a7a52c0a4a89a287258804c5f9c07b75adb
Oz Ramos <heyozramos@gmail.com>
2019-11-30 18:12:26 -0800

    #29 Adds virtual keyboard with big buttons

--- demo/views/VirtualKeyboard.vue
+++ demo/views/VirtualKeyboard.vue
@@ -1,7 +1,21 @@
 <template lang="pug">
-  h1 Virtual Keyboard  
+  div
+    v-container
+      v-layout
+        v-row
+          v-col.col-12.col-lg-4
+            v-card
+              v-card-title Virtual Keyboard 
+              v-card-text
+                p This demo explores the idea of typing on a virtual keyboard with face gestures.
+          v-col.col-12
+            SimpleKeyboard
 </template>
 
 <script>
-export default {}
+import SimpleKeyboard from '@/components/SimpleKeyboard'
+
+export default {
+  components: { SimpleKeyboard }
+}
 </script>
