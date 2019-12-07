ccd2b3e705a3be96278630b82f13e21b24f2dd18
Oz Ramos <heyozramos@gmail.com>
2019-12-06 21:04:56 -0800

    #36 Adds a lighter weight model for testing purposes

--- src/models/BodyPix.js
+++ src/models/BodyPix.js
@@ -1,11 +1,18 @@
 const Handsfree = window.Handsfree
 import * as bodyPix from '@tensorflow-models/body-pix'
 
+/**
+ * Begins loading BodyPix
+ */
 Handsfree.prototype.loadBodyPix = async function() {
   if (!this.config.models.bodypix.sdk) {
-    console.log('LOADING BODYPIX...')
-    this.config.models.bodypix.sdk = await bodyPix.load()
-    console.log('...READY')
+    this.config.models.bodypix.sdk = await bodyPix.load({
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      multiplier: 0.75,
+      quantBytes: 2
+    })
+
     this.emit('dependenciesReady')
   }
 }
