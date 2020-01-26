5a9f7d8f246bdbeaf83e1eeaf1a6f07bd2fb09ef
Oz Ramos <heyozramos@gmail.com>
2020-01-26 11:01:46 -0800

    #46 Collect samples for models

--- src/GestureRecorder.js
+++ src/GestureRecorder.js
@@ -24,7 +24,7 @@ Handsfree.prototype.createGestureRecorderOverlay = function(opts) {
  */
 Handsfree.prototype.recordGesture = function(opts) {
   // Setup opts
-  this.gestureRecorder.opts = opts = merge(
+  opts = merge(
     {
       gestureName: '',
       delaySeconds: 3,
@@ -41,29 +41,37 @@ Handsfree.prototype.recordGesture = function(opts) {
   opts.models.forEach((model) => {
     this.model[model].enabled = true
   })
+  this.gestureRecorder.opts = opts
   this.reload()
+  handsfree.start()
 
   // Countdown Message
-  let timesLooped = 0
-  const interval = setInterval(() => {
+  const countdown = () => {
     let message = this.config.gestureRecorder.countdownMessage.replace(
       /\{countdown\}/g,
       opts.delaySeconds - timesLooped
     )
     this.gestureRecorder.$message.innerHTML = message
+  }
 
-    if (++timesLooped === opts.delaySeconds) {
+  // Enable gestureRecorder plugin
+  let timesLooped = 0
+  countdown()
+  const interval = setInterval(() => {
+    ++timesLooped
+    countdown()
+
+    if (timesLooped >= opts.delaySeconds) {
       clearInterval(interval)
       this.gestureRecorder.samples = []
       Handsfree.enable('gestureRecorder.collectSample')
-      handsfree.start()
     }
   }, 1000)
 
   this.gestureRecorder.wrap.classList.add('handsfree-visible')
   document.body.classList.add('handsfree-recording-gesture')
 
-  // Disable calibration on calibration
+  // End gesture recording
   this.on('handsfreeGestureRecordingEnded', () => {
     Handsfree.disable('gestureRecorder.collectSample')
     document.body.classList.remove('handsfree-recording-gesture')
@@ -75,7 +83,56 @@ Handsfree.prototype.recordGesture = function(opts) {
  * Recursively collect gesture samples
  */
 Handsfree.use('gestureRecorder.collectSample', {
-  onFrame() {
-    console.log('collecting sample')
+  enabled: false,
+
+  onFrame({ config, head, body, emit, gestureRecorder }) {
+    let data = []
+
+    // Exit if models aren't active yet
+    if (
+      gestureRecorder.opts.models.includes('head') &&
+      !head.translation.length
+    )
+      return
+    if (gestureRecorder.opts.models.includes('bodypix') && !body.pose.keypoints)
+      return
+
+    // Loop through each model and store data in a flattened array
+    gestureRecorder.opts.models.forEach((model) => {
+      switch (model) {
+        case 'head':
+          Array.prototype.push.apply(
+            data,
+            head.translation,
+            head.rotation,
+            head.morphs
+          )
+          break
+
+        case 'bodypix':
+          let flatKeypoints = []
+          body.pose.keypoints.forEach((keypoint) => {
+            flatKeypoints.push(keypoint.x)
+            flatKeypoints.push(keypoint.y)
+          })
+
+          Array.prototype.push.apply(data, flatKeypoints)
+          break
+      }
+    })
+    gestureRecorder.samples.push(data)
+
+    // Update message
+    let message = config.gestureRecorder.recordingMessage.replace(
+      /\{numSamples\}/g,
+      gestureRecorder.samples.length
+    )
+    gestureRecorder.$message.innerHTML = message
+
+    // Finish recording after reaching target samples
+    if (gestureRecorder.samples.length >= gestureRecorder.opts.numSamples) {
+      Handsfree.disable('gestureRecorder.collectSample')
+      emit('handsfreeGestureRecordingEnded')
+    }
   }
 })
