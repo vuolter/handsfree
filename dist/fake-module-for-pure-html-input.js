ab59f809912ce90e0e263304c64e0611c0f1c1c8
Midiblocks <midiblocks@gmail.com>
2020-11-16 00:29:46 -0800

    #60 Removes livereload from build, updates version

--- dist/fake-module-for-pure-html-input.js
+++ dist/fake-module-for-pure-html-input.js
@@ -1,5 +1,3 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35730/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 const doNothing = true; // I am here to allow pure html inputs for rollup
 
 export { doNothing };
