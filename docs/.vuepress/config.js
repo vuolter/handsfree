403f7c7ca0b3c79db86a7ed4666d7c3c14ca4b35
Midiblocks <midiblocks@gmail.com>
2020-11-18 13:23:38 -0800

    #61 Adds Vuepress, sidebar, and template pages

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  themeConfig: {
+    sidebar: [
+      ['/', 'Home'],
+      {
+        title: '📚 Documentation',
+        path: '/docs/',
+        collapsable: true,
+        sidebarDepth: 1,
+        children: [
+          ['/docs/face/', '😀 Face Tracking'],
+          ['/docs/hand/', '🖖 Hand Tracking'],
+          ['/docs/pose/', '🤺 Pose Tracking']    
+        ]
+      },
+      ['/mapper/', '💻 Gesture mapper'],
+      ['/webhook/', '🔌 Connect to Webhook'],
+      ['/community/', '💜 Get Involved']
+    ]
+  }
+}
