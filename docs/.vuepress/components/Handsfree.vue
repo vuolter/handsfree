183d3998567231053dd3ee10ac68a8346b930a6d
Midiblocks <midiblocks@gmail.com>
2020-12-12 10:25:15 -0800

    #72 Removes NavBar override component, injects toggle button with JS

--- docs/.vuepress/components/Handsfree.vue
+++ docs/.vuepress/components/Handsfree.vue
@@ -1,16 +1,28 @@
 <template lang="pug">
 #handsfree-container
   #handsfree-debugger
+
+  HandsfreeToggle#navbar-handsfree-toggle(text-off='Activate Handsfree Mode' text-on='Stop Handsfree')
 </template>
 
 <script>
+import HandsfreeToggle from './HandsfreeToggle'
+
 /**
  * A global component designed to setup Handsfree.js
+ * - Adds the Handsfree Toggle to navbar
  */
 export default {
   name: 'HandsfreeContainer',
 
+  data () {
+    return {
+      hasMovedToggle: false
+    }
+  },
+
   mounted () {
+    // Import Handsfree
     if (!window.Handsfree) {
       import('@handsfree/handsfree.js').then(module => {
         const Handsfree = module.default
@@ -29,6 +41,19 @@ export default {
         })
         window.app = this.$root
       })
+    }
+  },
+
+  updated () {
+    if (!this.hasMovedToggle) {
+      // Add toggle to navbar
+      this.$nextTick(() => {
+        document.querySelector('header.navbar > .links').appendChild(
+          document.querySelector('#navbar-handsfree-toggle')
+        )
+      })
+
+      this.hasMovedToggle = true
     }
   }
 }
