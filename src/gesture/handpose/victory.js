4c4593b7dbfb3a9611405ce3f8cde8d72ecbf01c
Oz Ramos <midiblocks@gmail.com>
2021-01-29 14:37:12 -0800

    #107 Creates gesture definitions for ✌ and 👎

---
+++ src/gesture/handpose/victory.js
@@ -0,0 +1,28 @@
+// ✌
+export default [
+  ["addCurl", "Thumb", "HalfCurl", 0.5],
+  ["addCurl", "Thumb", "NoCurl", 0.5],
+  ["addDirection", "Thumb", "VerticalUp", 1.0],
+  ["addDirection", "Thumb", "DiagonalUpLeft", 1.0],
+  
+  ["addCurl", "Index", "NoCurl", 1.0],
+  ["addDirection", "Index", "VerticalUp", 0.75],
+  ["addDirection", "Index", "DiagonalUpLeft", 1.0],
+  
+  ["addCurl", "Middle", "NoCurl", 1.0],
+  ["addDirection", "Middle", "VerticalUp", 1.0],
+  ["addDirection", "Middle", "DiagonalUpLeft", 0.75],
+  
+  ["addCurl", "Ring", "FullCurl", 1.0],
+  ["addDirection", "Ring", "VerticalUp", 0.2],
+  ["addDirection", "Ring", "DiagonalUpLeft", 1.0],
+  ["addDirection", "Ring", "HorizontalLeft", 0.2],
+  
+  ["addCurl", "Pinky", "FullCurl", 1.0],
+  ["addDirection", "Pinky", "VerticalUp", 0.2],
+  ["addDirection", "Pinky", "DiagonalUpLeft", 1.0],
+  ["addDirection", "Pinky", "HorizontalLeft", 0.2],
+  
+  ["setWeight", "Index", 2],
+  ["setWeight", "Middle", 2],
+]
